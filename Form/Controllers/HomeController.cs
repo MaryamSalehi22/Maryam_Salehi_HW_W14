e092e202acf75c75ae6bda5a56e58ce20e79dfb9
maryamsalehi22148 <tiamsalehi22148@gmail.com>
2024-07-29 02:27:25 -0700

    Add project files.

---
+++ Form/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using Form.Models;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Form.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Subscription()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Subscription(FormViewModel form)
+        {
+            if (!ModelState.IsValid || !form.IsValidAge())
+            {
+                if (!form.IsValidAge())
+                {
+                    ModelState.AddModelError(nameof(form.BirthDate), "You must be at least 18 years old.");
+                }
+                return View("Subscription", form);
+            }
+
+            return Json("Subscription", form);
+
+        }
+        public IActionResult Rules()
+        {
+            return View();
+        }
+
+    }
+}
+
+
---
+++ Form/Models/FormViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Form.Models
+{
+    public class FormViewModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 30 characters.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Last Name must be between 3 and 30 characters.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [RegularExpression(@"^09\d{9}$", ErrorMessage = "Phone number must start with 09 and be 11 digits long.")]
+        public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date)]
+        public DateOnly BirthDate { get; set; }
+        [Required(ErrorMessage = "Course Code is required.")]
+        [Range(100, 999, ErrorMessage = "Course Code must be a three-digit number.")]
+        [RegularExpression(@"^[02468]\d{2}$", ErrorMessage = "Course code must be a three-digit number with an even hundreds digit.")]
+        public int CourseCode { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "You must accept the rules.")]
+        public bool Checked { get; set; }
+
+        public bool IsValidAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year >= 18;
+        }
+    }
+}
